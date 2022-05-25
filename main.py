cc5e953883dde25908a8145e517b7aea024e5936
Oleksandra Melnyk <oleksandramelnyk.ibm@gmail.com>
2022-05-25 13:17:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 data = {}
 
-command = []
+contacts = {}
 
 def input_error(func):
     def wrapper(*args, **kwargs):
@@ -37,7 +37,7 @@ def input_add(*args):
 @input_error
 def input_change(*args):
     data[args[0]] = args[1]
-    return f"Contact {args[0].title()} changeв"
+    return f"Contact {args[0].title()} changed"
 
 
 @input_error
@@ -45,12 +45,11 @@ def input_phone(*args):
     return data[args[0]]
 
 def input_show():
-    contacts = []
     for k, v in data.items():
         _ = k.title() + ' ' + str(v)
         contacts.append(_)
         contacts_join = ('\n').join(contacts)
-    return f'All contacts:\n{contacts_join}'
+    return "\n".join([f"{k}: {v} " for k, v in contacts_join.items()])
 
 
 COMMANDS = {
@@ -61,36 +60,33 @@ COMMANDS = {
     input_change: "change",
     exit_bot: "good bye",
     close_bot: "close",
-    bye_bot: "exit",
+    bye_bot: "exit"
 }
 
 def main():
     while True:
         user_input = input(">>> ")
         for k, v in COMMANDS.items():
-        if v == user_input:
-            print(k())
-        if user_input == '.':
-            break
-        user_input = user_input.lower()
-        for k, v in COMMANDS.items():
-            if user_input.startswith(v):
+            if v == user_input:
                 print(k())
+            if user_input == '.':
+                break
+            user_input = user_input.lower()
             if user_input in ['good bye', 'exit', 'close']:
                 False
-        cmd = user_input.split(' ')
-        if cmd[0] == 'add':
-            command.extend(cmd)
-            print(input_add())
-            command.clear()
-        if cmd[0] == 'change':
-            command.extend(cmd)
-            print(input_change())
-            command.clear()
-        if cmd[0] == 'phone':
-            command.extend(cmd)
-            print(input_phone(), end='\n')
-            command.clear()
+            cmd = user_input
+            if cmd[0] == 'add':
+                contacts.extend(cmd)
+                print(input_add())
+                contacts.clear()
+            if cmd[0] == 'change':
+                contacts.extend(cmd)
+                print(input_change())
+                contacts.clear()
+            if cmd[0] == 'phone':
+                contacts.extend(cmd)
+                print(input_phone(), end='\n')
+                contacts.clear()
 
 
 if __name__ == "__main__":
